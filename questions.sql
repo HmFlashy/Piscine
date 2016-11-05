e81903d8b9311a81eddfa8361fa9a1dd6d1974f3
Leesanwei <lswei93@hotmail.com>
2016-11-05 12:55:06 +0100

    Add files via upload

---
+++ questions.sql
@@ -0,0 +1,144 @@
+CREATE TABLE IF NOT EXISTS questions(
+id_question INTEGER NOT NULL AUTO_INCREMENT,
+question varchar(100) NOT NULL,
+ PRIMARY KEY(id_question)
+) ENGINE=InnoDB CHARSET=utf8;
+
+
+
+INSERT INTO questions(question) VALUES ("Vous aimez avoir des activités à l’extérieur, travailler en plein air ");
+INSERT INTO questions(question) VALUES ("Vous aimez élargir vos connaissances par l’étude, pouvoir approfondir un sujet ");
+INSERT INTO questions(question) VALUES ("Vous aimez travailler de façon indépendante, sans méthode ni objectif structurés");
+INSERT INTO questions(question) VALUES ("Vous aimez travailler avec d’autres personnes pour les informer ");
+INSERT INTO questions(question) VALUES ("Vous admirez les personnes qui ont du pouvoir ou gagnent beaucoup d’argent");
+INSERT INTO questions(question) VALUES ("Vous aimez travailler avec des chiffres ");
+INSERT INTO questions(question) VALUES ("Vous admirez les personnes qui travaillent pour soigner les autres ");
+INSERT INTO questions(question) VALUES ("Vous aimez une organisation claire et bien définie ");
+INSERT INTO questions(question) VALUES ("Vous aimez contribuer à atteindre les objectifs d’une organisation 
+");
+INSERT INTO questions(question) VALUES ("Vous aimez le sport, vous dépenser physiquement 
+");
+INSERT INTO questions(question) VALUES ("Vous aimez étudier les choses, les phénomènes ou les comportements 
+");
+INSERT INTO questions(question) VALUES ("Vous admirez les personnes qui ont des capacités artistiques 
+");
+INSERT INTO questions(question) VALUES ("Vous aimez travailler avec d’autres personnes pour les former, les entraîner 
+");
+INSERT INTO questions(question) VALUES ("Vous aimez les changements ou les situations imprévues 
+");
+INSERT INTO questions(question) VALUES ("Vous aimez ne faire qu’une seule chose à la fois et vous ne vous laissez pas distraire 
+");
+INSERT INTO questions(question) VALUES ("Vous aimez donner des ordres ou consignes et organiser l’activité des autres 
+");
+INSERT INTO questions(question) VALUES ("Vous aimez tirer vos propres conclusions de l’analyse d’une situation donnée 
+");
+INSERT INTO questions(question) VALUES ("Vous aimez conduire des véhicules ou faire fonctionner des machines 
+");
+INSERT INTO questions(question) VALUES ("Vous aimez fabriquer ou réparer des objets 
+");
+INSERT INTO questions(question) VALUES ("Vous aimez ne pas savoir précisément ce que vous avez à faire 
+");
+INSERT INTO questions(question) VALUES ("Vous aimez mettre en œuvre des " bonnes pratiques " acquises par l’expérience 
+");
+INSERT INTO questions(question) VALUES ("Vous aimez faire preuve d’initiative et prendre des décisions rapides 
+");
+INSERT INTO questions(question) VALUES ("Vous aimez écouter, dialoguer, essayer de comprendre les autres 
+");
+INSERT INTO questions(question) VALUES ("Vous aimez vous fier à votre jugement pour décider comment faire les choses 
+");
+INSERT INTO questions(question) VALUES ("Vous aimez faire plusieurs activités en même temps, ou passer d’une action à l’autre 
+");
+INSERT INTO questions(question) VALUES ("Vous aimez décider de ce qui doit être fait 
+");
+INSERT INTO questions(question) VALUES ("Vous aimez rencontrer des gens nouveaux 
+");
+INSERT INTO questions(question) VALUES ("Vous aimez vérifier une conclusion par des tests ou des informations complémentaires 
+");
+INSERT INTO questions(question) VALUES ("Vous aimez appuyer vos conclusions sur des bases déjà prouvées 
+");
+INSERT INTO questions(question) VALUES ("Vous aimez bricoler, utiliser des outils tels que tournevis, ciseaux, pinces, etc.... 
+");
+INSERT INTO questions(question) VALUES ("Vous aimez résoudre les problèmes de façon rationnelle, étape par étape 
+");
+INSERT INTO questions(question) VALUES ("Vous aimez la nature, les plantes, les animaux... 
+");
+INSERT INTO questions(question) VALUES ("Vous aimez respecter les valeurs que vous vous êtes fixées 
+");
+INSERT INTO questions(question) VALUES ("Vous aimez faire un travail en commun avec d’autres 
+");
+INSERT INTO questions(question) VALUES ("Vous aimez relever des défis 
+");
+INSERT INTO questions(question) VALUES ("Vous aimez vous fier à votre intuition pour prendre des décisions 
+");
+INSERT INTO questions(question) VALUES ("Vous aimez convaincre les autres d’agir d’une certaine façon 
+");
+INSERT INTO questions(question) VALUES ("Vous aimez résoudre un problème sans avoir recours à une méthode logique 
+");
+INSERT INTO questions(question) VALUES ("Vous aimez prendre une décision après une réflexion, si possible logique 
+");
+INSERT INTO questions(question) VALUES ("Vous aimez suivre attentivement un plan pour atteindre le meilleur résultat possible 
+");
+INSERT INTO questions(question) VALUES ("Vous aimez écouter les autres et les conseiller sur la façon de résoudre leurs problèmes 
+");
+INSERT INTO questions(question) VALUES ("Vous aimez trouver une solution concrète, réaliste et simple aux problèmes 
+");
+INSERT INTO questions(question) VALUES ("Vous aimez concevoir ou améliorer les méthodes de travail 
+");
+INSERT INTO questions(question) VALUES ("Vous aimez utiliser votre " bon sens " 
+");
+INSERT INTO questions(question) VALUES ("Vous aimez rendre service, venir en aide à d’autres personnes 
+");
+INSERT INTO questions(question) VALUES ("Vous aimez répondre aux objections de vos interlocuteurs pour mieux les convaincre 
+");
+INSERT INTO questions(question) VALUES ("Vous aimez montrer votre originalité 
+");
+INSERT INTO questions(question) VALUES ("Vous aimez travailler avec soin pour obtenir un résultat parfait 
+");
+INSERT INTO questions(question) VALUES ("Vous aimez ou aimeriez animer des activités collectives, associatives... 
+");
+INSERT INTO questions(question) VALUES ("Vous aimez ou aimeriez étudier la physique, la biologie, ou la technologie 
+");
+INSERT INTO questions(question) VALUES ("Vous aimez démonter un appareil pour le réparer vous-même 
+");
+INSERT INTO questions(question) VALUES ("Vous aimez discuter avec un commerçant pour obtenir des réductions de prix 
+");
+INSERT INTO questions(question) VALUES ("Vous aimez exprimer vos idées, vos points de vue ou vos émotions 
+");
+INSERT INTO questions(question) VALUES ("Vous aimez rédiger un résumé, une lettre, un compte-rendu 
+");
+INSERT INTO questions(question) VALUES ("Vous aimez faire face aux situations urgentes ou imprévues 
+");
+INSERT INTO questions(question) VALUES ("Vous aimez vous occuper de démarches administratives ou d’ordre juridique 
+");
+INSERT INTO questions(question) VALUES ("Vous aimez ou aimeriez faire des reportages, écrire des articles, etc... 
+");
+INSERT INTO questions(question) VALUES ("Vous aimez chercher à comprendre et à expliquer le pourquoi des choses et des êtres 
+");
+INSERT INTO questions(question) VALUES ("Vous aimez imaginer des solutions qui sortent de l’ordinaire 
+");
+INSERT INTO questions(question) VALUES ("Vous aimez ou aimeriez utiliser un objet que vous avez fabriqué vous-même 
+");
+INSERT INTO questions(question) VALUES ("Vous aimez apprendre aux autres ce que vous savez 
+");
+INSERT INTO questions(question) VALUES ("Vous aimez collectionner des choses : timbres, cartes postales, pierres, etc.... 
+");
+INSERT INTO questions(question) VALUES ("Vous aimez passer une grande partie de votre temps sur des documents écrits 
+");
+INSERT INTO questions(question) VALUES ("Vous aimez ou aimeriez vendre des produits ou services 
+");
+INSERT INTO questions(question) VALUES ("Vous aimez vous servir d’un microscope ou autre appareil de mesure... 
+");
+INSERT INTO questions(question) VALUES ("Vous aimez ou aimeriez avoir des loisirs créatifs : peinture, musique... 
+");
+INSERT INTO questions(question) VALUES ("Vous aimez classer, ordonner des documents ou des objets 
+");
+INSERT INTO questions(question) VALUES ("Vous aimez conduire une discussion, un débat 
+");
+INSERT INTO questions(question) VALUES ("Vous aimez échanger des idées avec les autres 
+");
+INSERT INTO questions(question) VALUES ("Vous aimez que ce que vous faites débouche sur des résultats concrets 
+");
+INSERT INTO questions(question) VALUES ("Vous aimez ou aimeriez mettre au point et réaliser des expériences scientifiques 
+");
+INSERT INTO questions(question) VALUES ("Vous aimez étudier ou inventer plusieurs solutions pour répondre à un problème 
+");
