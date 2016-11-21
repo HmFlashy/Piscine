02a3e2af85fe1d55ab0cd46e7466a331258ea2e3
HmFlashy <hugo.maitre69@gmail.com>
2016-11-21 19:24:46 +0100

    test marche pas

--- includes/entete.php
+++ includes/entete.php
@@ -1,6 +1,6 @@
 <div id='entete' class='container'>
 	<div class="page-header" style="text-align: center;">
-			<h1 id="grostitre">TEST DE HOLLANDE lolilol
+			<h1 id="grostitre">TEST DE HOLLANDE
 			<?php 
 			session_start();
 			session_set_cookie_params(0);
