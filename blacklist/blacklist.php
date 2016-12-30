fbda0c40d9bad8e1219cb7ced7c430a413fef5ee
alexbottero <alex.bottero@hotmail.fr>
2016-12-30 23:11:19 +0100

    Add files via upload

--- blacklist/blacklist.php
+++ blacklist/blacklist.php
@@ -1,4 +1,4 @@
 <?php
-$blacklistProf = array('accueilProf', 'choixDepartement', 'choixPromotion', 'generationCode', 'gererPromos', 'supprimerCode', 'riasec', 'test','mdp','secret', 'genererSession');
+$blacklistProf = array('accueilProf', 'choixDepartement', 'choixPromotion', 'generationCode', 'gererPromos', 'supprimerCode', 'riasec', 'test','mdp','secret', 'genererSession','statistiques');
 $blacklistEleve = array('acceuilEtu', 'promoPerso', 'rejoindrePromo', 'resultatTest', 'riasec', 'test');
 ?>
