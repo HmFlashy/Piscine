0614947fc0a0eaaa43ca3bd2f8646ed1404f21ad
Leesanwei <lswei93@hotmail.com>
2017-01-05 03:12:04 +0100

    Titre pour page d'accueil

--- includes/entete.php
+++ includes/entete.php
@@ -1,5 +1,5 @@
 <div id='entete' class="page-header" style="text-align: center;">
-		<h1 id="grostitre">TEST DE HOLLAND<br><small>(MODELE RIASEC)</small></h1>
+		<h1 id="grostitre"><a href="http://localhost/piscine/?" style="color:inherit;text-decoration: none">TEST DE HOLLAND<br><small>(MODELE RIASEC)</small></a></h1>
 		<?php 
 		session_start();
 		session_set_cookie_params(0);
